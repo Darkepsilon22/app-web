a76687db0d6c51914c131c74ba062a0bb77bf853
Darkepsilon22 <yohanrak61@gmail.com>
2025-02-05 08:38:15 +0300

    update graphe

--- sql/data.sql
+++ sql/data.sql
@@ -1,15 +1,5 @@
 -- Active: 1726508341924@@127.0.0.1@3306@appcrypto
-INSERT INTO crypto (intitule, current_valeur) VALUES
-('Bitcoin', 50000.00),
-('Ethereum', 4000.00),
-('Binance Coin', 350.00),
-('Ripple', 1.00),
-('Cardano', 1.20),
-('Solana', 150.00),
-('Polkadot', 25.00),
-('Dogecoin', 0.30),
-('Litecoin', 180.00),
-('Chainlink', 30.00);
+    
 
 INSERT INTO users (prenom, nom, date_naissance, email, password, solde, date_inscription)
 VALUES
