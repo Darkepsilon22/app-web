eff5d56cf1bdf810d5897d1912853bb2f3a374ef
Darkepsilon22 <yohanrak61@gmail.com>
2025-02-06 14:36:36 +0300

    update  base

--- sql/crypto.sql
+++ sql/crypto.sql
@@ -9,15 +9,23 @@ CREATE TABLE users(
    email VARCHAR(255)  NOT NULL,
    password VARCHAR(255)  NOT NULL,
    nom VARCHAR(255)  NOT NULL,
-   solde NUMERIC(15, 2), 
+   solde NUMERIC(19,5),
    date_inscription TIMESTAMP NOT NULL,
    PRIMARY KEY(Id_users),
    UNIQUE(email)
 );
 
+CREATE TABLE users_admin(
+   Id_users_admin SERIAL,
+   username VARCHAR(255)  NOT NULL,
+   password VARCHAR(255)  NOT NULL,
+   PRIMARY KEY(Id_users_admin),
+   UNIQUE(username)
+);
+
 CREATE TABLE mouvement_solde(
    Id_mouvement_solde SERIAL,
-   somme NUMERIC(15, 2) NOT NULL, 
+   somme NUMERIC(19,5) NOT NULL,
    date_mouvement TIMESTAMP NOT NULL,
    est_depot BOOLEAN NOT NULL,
    Id_users INTEGER NOT NULL,
@@ -28,41 +36,30 @@ CREATE TABLE mouvement_solde(
 CREATE TABLE crypto(
    Id_crypto SERIAL,
    intitule VARCHAR(50)  NOT NULL,
-   current_valeur NUMERIC(15, 2),
+   current_valeur NUMERIC(19,5),
    PRIMARY KEY(Id_crypto)
 );
 
 CREATE TABLE cour_crypto(
    Id_cour_crypto SERIAL,
    instant TIMESTAMP NOT NULL,
-   valeur_dollar NUMERIC(15, 2) NOT NULL,
+   valeur_dollar NUMERIC(19,5) NOT NULL,
    Id_crypto INTEGER NOT NULL,
    PRIMARY KEY(Id_cour_crypto),
    FOREIGN KEY(Id_crypto) REFERENCES crypto(Id_crypto)
 );
 
-CREATE TABLE mouvement_crypto (
+CREATE TABLE mouvement_crypto(
    Id_mouvement_crypto SERIAL,
    est_achat BOOLEAN NOT NULL,
+   date_mouvement TIMESTAMP NOT NULL,
+   quantite NUMERIC(15,8)   NOT NULL,
    Id_crypto INTEGER NOT NULL,
    Id_users INTEGER NOT NULL,
-   date_mouvement TIMESTAMP NOT NULL,
    PRIMARY KEY(Id_mouvement_crypto),
    FOREIGN KEY(Id_crypto) REFERENCES crypto(Id_crypto),
    FOREIGN KEY(Id_users) REFERENCES users(Id_users)
 );
-
-
-CREATE TABLE crypto_utilisateur (
-   Id_crypto_utilisateur SERIAL,
-   quantite NUMERIC(15, 2) NOT NULL, 
-   Id_users INTEGER NOT NULL,
-   Id_crypto INTEGER NOT NULL,
-   PRIMARY KEY(Id_crypto_utilisateur),
-   FOREIGN KEY(Id_users) REFERENCES users(Id_users),
-   FOREIGN KEY(Id_crypto) REFERENCES crypto(Id_crypto)
-);
-
 
 CREATE TABLE token_connexion(
    Id_token_connexion SERIAL,
@@ -73,3 +70,31 @@ CREATE TABLE token_connexion(
    UNIQUE(code),
    FOREIGN KEY(Id_users) REFERENCES users(Id_users)
 );
+
+CREATE TABLE commission(
+   Id_commission SERIAL,
+   pourcentage NUMERIC(15,2)   NOT NULL,
+   valeur NUMERIC(15,2)   NOT NULL,
+   Id_mouvement_crypto INTEGER NOT NULL,
+   PRIMARY KEY(Id_commission),
+   FOREIGN KEY(Id_mouvement_crypto) REFERENCES mouvement_crypto(Id_mouvement_crypto)
+);
+
+CREATE TABLE historique_commission(
+   Id_historique_commission SERIAL,
+   date_historique TIMESTAMP NOT NULL,
+   valeur_historique NUMERIC(15,2)   NOT NULL,
+   PRIMARY KEY(Id_historique_commission)
+);
+
+CREATE TABLE crypto_utilisateur(
+   Id_crypto_utilisateur SERIAL,
+   quantite NUMERIC(15,8)   NOT NULL,
+   Id_users INTEGER NOT NULL,
+   Id_crypto INTEGER NOT NULL,
+   PRIMARY KEY(Id_crypto_utilisateur),
+   FOREIGN KEY(Id_users) REFERENCES users(Id_users),
+   FOREIGN KEY(Id_crypto) REFERENCES crypto(Id_crypto)
+);
+
+
